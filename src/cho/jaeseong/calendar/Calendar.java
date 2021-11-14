3abcb8c8e7eebc9c665fdf328c84116c0dded328
is2js <tingstyle1@gmail.com>
2021-11-14 22:28:11 +0900

    println으로만 달력찍어보기

--- src/cho/jaeseong/calendar/Calendar.java
+++ src/cho/jaeseong/calendar/Calendar.java
@@ -2,6 +2,20 @@ package cho.jaeseong.calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		// 1 . 달력을  println에 문자열 그대로 찍어보기
+		//- https://gist.github.com/is2js/3a058b8dde226958b6d2253c3c278ac0
+		//- 과제 문자열에 앞뒤로 sysout코드를 복붙해서 만들어줬음.
+//		일 월 화 수 목 금 토
+//		--------------------
+//		 1  2  3  4  5  6  7
+//		 8  9 10 11 12 13 14
+//		15 16 17 18 19 20 21
+//		22 23 24 25 26 27 28
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 }
