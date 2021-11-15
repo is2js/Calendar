b8080d4091e035ba60f28e73b5838f1952662a30
is2js <tingstyle1@gmail.com>
2021-11-15 18:08:49 +0900

    Calendar 윤년판단 로직을 포함해서 일요일을 1일부터 maxDay까지 달력출력하기 + Prompt에서  year도 받도록 수정하기

--- src/cho/jaeseong/calendar/Calendar.java
+++ src/cho/jaeseong/calendar/Calendar.java
@@ -4,62 +4,49 @@ import java.util.Scanner;
 
 /*
  *요구사항
-월을 입력하면 해당월의 달력을 출력한다.
-달력은 모양은 1단계에서 작성한 모양으로 만든다.
-1일은 일요일로 정해도 무방하다. 			-> 쉽게 풀려고 하는 것
--1을 입력받기 전까지 반복 입력받는다.
-프롬프트를 출력한다.
-
-
-입력 및 출력 예시
-월을 입력하세요.
-> 3
-일 월 화 수 목 금 토
---------------------
- 1  2  3  4  5  6  7
- 8  9 10 11 12 13 14
-15 16 17 18 19 20 21
-22 23 24 25 26 27 28
-29 30 31
-월을 입력하세요.
-> -1
-Bye~ 
+ *윤년판단 로직을 포함해서 일요일->1일부터 달력 출력하기
  */
 
 public class Calendar {
 	
+	//1. 윤년용 MAX_DAYS 배열을 1개더 선언해서, 윤년시 해당배열을 사용하도록 하면 된다. (2월 28일 -> 29일)
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-	public int getMaxDaysOfMonth(int month) {
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	//2. 2개의 배열중 어떤배열쓸지를 int year에 의해 결정되며, 여기 class는 그 판단유무를 결정해숴 반환해줘야한다!!!
+	// 그걸 판단해서 반환해주는 boolean 메서드를 여기 class에서 정의하고, 사용처에서 사용해준다. ( 외부면 cal.isLeapYear 내부면 this.isLeapyear)
+	// - 에러가 짜증나니까, 미리 return false;로 해놓고 작성하자.
+	public boolean isLeapYear(int year) {
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 )) {
+			return true;
+		}
+		return false;
+	}
+	
+	//3. 윤년판단로직은 getMaxDaysofMonth에서 사용해서, 사용할 배열을 결정하여 maxday를 반환해줘야한다.
+	// -> isLeapYear(year) 사용해야하므로 year도 받아주도록 수정해야한다.
+	// year가 사용되어야하므로, 매개변수에 int year를 추가해주자. -> 사용처에서도 추가해주자.
+//	public int getMaxDaysOfMonth(int month) {
+	//5. 
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month-1];
+		}
 		return MAX_DAYS[month-1];
 	}
 	
 	public void printCalendar(int year, int month) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
-		//2-4. 숫자가 오른쪽정렬 된 3칸을 차지하므로,, 맨앞에 공백을 추가해주자. []SU가 1개 단위 ->  다음줄에도 -를 하ㅏㄴ 추가해주자.
 		System.out.println(" SU MO TU WE TH FR SA");		
 		System.out.println("---------------------");
-		//1. 이 아래부분부터 하드코딩(주석처리)이 아니라 계산해서 구해야한다. 먼저 maxDay가 필요함.
-		// - 같은 클래스내이므로, cal.메소드()가 아니라, (static없는 인스턴스메소든데???)도 메소드()로 호출한다?
-		// -> 같은 클래스라면, this없이도? 인스턴스메소드라도 편하게 구할 수 있다?
-		// -> 원래는 cal.~으로 호출되어야하지만, 같은 내부클래스라면 객체생략(this생략)하고 호출되나보다.
-		int maxDay = this.getMaxDaysOfMonth(month);
-		
-		//2. 1부터 maxDay까지만 출력하면 된다. -> 일단 여기까지 출력해보기
-		for (int i = 1; i < maxDay; i++) {
-			System.out.printf("%3d", i); // 2-1.공백포함 3글자를 차지하도록 숫자를 나열한다.
-			// 2-3. 줄바꿈은 7개 마다 해야한다. -> 7로 나눈 나머지가?? 첫항1 기준으로 총 7개째에 줄바꿈 -> 7일때 줄바꿈 -> 나머지%7이 0일 때 줄바꿈
+		int maxDay = this.getMaxDaysOfMonth(year, month); // 4. 사용처에 year추가. -> F3으로 정의부 돌아가기
+		for (int i = 1; i <= maxDay; i++) { // 1부터 시작이므로 <= N으로 수정함..
+			System.out.printf("%3d", i);
 			if (i % 7 == 0 ) {
 				System.out.println();
 			}
 		}
-		//2-2. printf를 모으는데 사용할 거면, 다 모으고나서 -> 개별로 println추가 넣어줘야한다.
 		System.out.println();
-		
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 	}
 	
 	
--- src/cho/jaeseong/calendar/Prompt.java
+++ src/cho/jaeseong/calendar/Prompt.java
@@ -10,8 +10,17 @@ public class Prompt {
 		Calendar cal = new Calendar();
 
 		int month = 1;
-
+		//6. year도 받아서 사용하도록 하려면, 매 반복문마다 입력값이 달라져서 , month처럼, 미리 선언된 변수에 받아주는 변수가 필요하다.
+		// - 반복문에서 입력값을 받아주는 용도일 뿐이므로 초기값은 별로 안중요하다. -1만 아니면 된다.! (year는 딱히 종료조건을 안준다)
+		// my) 반복문에서 루프로 받는 input은, 매번 선언해줄수없은, 미리 밖에서 선언해놓고 -> 변수 재활용하면서 input을 받아줘야한다.
+		int year = 2000;
+		
 		while (true) {
+			//7.
+			System.out.println("연도를 입력하세요.");
+			System.out.print(PROMPT);
+			year = scanner.nextInt();
+			
 			System.out.println("달을 입력하세요.");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
@@ -22,7 +31,9 @@ public class Prompt {
 				System.out.println("1~12사이의 값을 입력하세요.");
 				continue;
 			}
-			cal.printCalendar(2021, month);
+			//8. 하드코딩 대신 입력받은 year가 들어간다.
+//			cal.printCalendar(2017, month);
+			cal.printCalendar(year, month);
 			
 		}
 		
