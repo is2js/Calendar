72582d02c40b29fdd449f7e39fefc572ede94651
is2js <tingstyle1@gmail.com>
2021-11-16 11:33:21 +0900

    요일입력안받고, 년월일로 시작요일 판단하기1 - 세팅(weekday argu -> parameter없애기)

--- src/cho/jaeseong/calendar/Calendar.java
+++ src/cho/jaeseong/calendar/Calendar.java
@@ -1,76 +1,54 @@
 package cho.jaeseong.calendar;
 
-//import java.util.Scanner;
-
 /*
- *요구사항
- *월을 입력받는다.
- *1일의 요일을 입력받는다.
- *출력한다.
- *
- *입력 및 출력 예시
-년도를 입력하세요.
-YEAR> 2017
-달을 입력하세요.
-MONTH> 3
-			첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)
-			WEEKDAY> WE
-			    <<2017년  3월>>
-			 SU MO TU WE TH FR SA
-			---------------------
-			           1  2  3  4
-			  5  6  7  8  9 10 11
+ * 요구사항
+3단계와 같지만 년도와 월만 입력받는다.
+진짜 캘린더에서 나오는 달력과 똑같은 모양의 달력을 출력한다.
+추가적으로 입력받아야 하는 내용이 있는지 생각해 보자
  */
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public boolean isLeapYear(int year) {
-		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 )) {
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public int getMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month-1];
+			return LEAP_MAX_DAYS[month - 1];
 		}
-		return MAX_DAYS[month-1];
+		return MAX_DAYS[month - 1];
 	}
-	// 7. weekday까지 받았으니 -> 시작요일만큼 앞에 공백을 넣어주도록 설계해야한다.
-	// -> 일요일(0)일때는 공백0, 월용ㄹ(1)일 때는 공백1개 ... weekday만큼 앞에 공백을 채워주면된다. 
-	public void printCalendar(int year, int month, int weekday) {
-		System.out.printf("   <<%4d년%3d월>>\n", year, month);
-		System.out.println(" SU MO TU WE TH FR SA");		
+	
+	//2. 입력받는 parameter의 weekday제거하기 
+	// -> but **내부에서 weekday는 계산에 의해 정해져야한다. 내부로직은 살려도록 weekday를 내부에서 하나 선언해놓자.**
+	//public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
+		System.out.printf("   <<%d년 %d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		int maxDay = this.getMaxDaysOfMonth(year, month); 
-		//8. 숫자출력 전에, print blank space(3묶음이 1개)
-		//print first line
-		for (int i = 0; i < weekday; i++ ) { 
+		int maxDay = this.getMaxDaysOfMonth(year, month);
+		// 3. parameter weekday는 없어진 대신 계산으로 weekday를 구해서 공백 출력해야한다. 일단 초기화해놓기
+		// TODO: get weekday automatically
+		int weekday = 0;
+		// 4. 2017년 6월 1일이 무슨요일인지.. 자동으로 알아내야하는데 복잡하므로 검색을 통해 가져와야한다. 		
+		
+		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
-//		   <<2021년  1월>>
-//		   SU MO TU WE TH FR SA
-//		  ---------------------
-//		                1  2  3  4  5  6  7
-		// print form second ine to last
-		for (int i = 1; i <= maxDay; i++) { 
+		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			//9. 이제 줄바꿈을 언제해줘야할지 생각해보기. 기준은 weeday고정한 상태로.. i가 언제마다 줄바꿈을 하는지.. 식 찾고.. %7==0으로 표현
-			// -> 원래는 i가 7마다 줄바꿈  -> weekday 3 -> 현  3개 덜가도 줄바꿈 -> 
-			// @@  weekday고정해놓고 줄바꿈이 일어나는 i 규칙찾기 @@
-			// weekday 3 -> 3개 공백후 1234에서 줄바꿈 -> 11에서 -> 18에서 --> @@ 7k-3 @@  i + 3= 7k -> i+weekday % 7 == 0마다 줄바꿈
-			// -> @@ 달력처럼 우측으로 몇칸땡겨서 줄바꿈해야된다?  그 줄바꿈의 빈도는 @@ if (day + 전진횟수) % 7 == 0 @@마다 줄바꿈하면 된다. 
-//			if (i % 7 == 0 ) {
 			if ((i + weekday) % 7 == 0) {
 				System.out.println();
 			}
 		}
 		System.out.println();
 	}
-	
-	
+
 }
--- src/cho/jaeseong/calendar/Prompt.java
+++ src/cho/jaeseong/calendar/Prompt.java
@@ -3,19 +3,12 @@ package cho.jaeseong.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	// 0. 안써서 삭제
-	//private final static String PROMPT = "cal> ";
-	
-	// 3. 문자열 -> 숫자로 변환해주는 메소드도 따로 뺀다. 
-	// - 일단 모두 static이 아닌 객체 생성(cal)해서 호출하는 인스턴스메서드(cal.메소드()) + public으로 쓰나보다.. cf) 변수는 클래스든 인스턴스든 다 public금지?
-	// 4. 헤깔리는 메소드는 /**enter로 문서주석을 단다.
 	/**
 	 * 
 	 * @param week 요일명
 	 * @return 0 ~ 6 (0 = Sunday, 6= Saturday)
 	 */
 	public int parseDay(String week) {
-		// 문자열 비교는 equals!!쓰기!!
 		if (week.equals("SU")) {
 			return 0;
 		}
@@ -36,34 +29,32 @@ public class Prompt {
 		}
 		return 6;
 	}
-	
-	public void runPrompt() { 
+
+	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int month = 1;
 		int year = 2000;
-		//2. weekday(요일)도 매 반복문마다 input-> 받아줄 변수가 필요해서 초기화해놓는다.
-		int weekday = 0; // 일요일을 0으로 보고 토요일 6으로 본다고 가정한다.
-		
+		//0. 요일안받으려고 제거
+		//int weekday = 0;
+
 		while (true) {
 			System.out.println("연도를 입력하세요.(exit:-1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
-			// 10. year도 받자마자 종료조건 필터링 부터 한다.
 			if (year == -1) {
 				break;
 			}
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			// 1. 첫째날의 요일(문자열) 입력받기 -> 문자열로 입력받아서 간단한 메소드를 통해  문자열->숫자로 변환 추출할 것이다. 
-			System.out.println("첫째 날의 요일을 입력하세요.(ex> SU, MO, TU, WE, TH, FR, SA)");
-			System.out.print("WEEKDAY> ");
-			String strWeekday= scanner.next();
-			// 5. 정의한 메소드 적용해주기
-			weekday = this.parseDay(strWeekday); // 인스턴스메소드는... 같은 클래스 메소드 내에선.. 객체 생성없이..this도 없이? 호출이 되나보다. 하지만 this.찍어서 호출해보자.
-			
+			//0. 요일안받으려고 제거
+			//System.out.println("첫째 날의 요일을 입력하세요.(ex> SU, MO, TU, WE, TH, FR, SA)");
+			//System.out.print("WEEKDAY> ");
+			//String strWeekday = scanner.next();
+			//weekday = this.parseDay(strWeekday);
+
 			if (month == -1) {
 				break;
 			}
@@ -71,20 +62,18 @@ public class Prompt {
 				System.out.println("1~12사이의 값을 입력하세요.");
 				continue;
 			}
-			
-			//6. 캘린더 출력메소드에.. weekday도 받아서 처리하게 인수(argument)수정 -> 7. 매개변수(parameter) 수정 by [F3]
-			//cal.printCalendar(year, month);
-			cal.printCalendar(year, month, weekday);
-			
+			//1. cal클래스 인스턴스메서드 printCalendar의 argument -> parameter에섣 weekday제거해줘야한다.
+			//cal.printCalendar(year, month, weekday);
+			cal.printCalendar(year, month);
 		}
-		
+
 		System.out.println("Bye~!");
 
 		scanner.close();
 	}
-	
+
 	public static void main(String[] args) {
-		Prompt p = new Prompt(); 
+		Prompt p = new Prompt();
 		p.runPrompt();
 	}
 
