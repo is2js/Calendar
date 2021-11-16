80dec0d9eada25b70816e75be593b95247a00209
is2js <tingstyle1@gmail.com>
2021-11-16 21:20:09 +0900

    일정등록/검색기능 구현1- UI +달력보기 구현완료

--- src/cho/jaeseong/calendar/Calendar.java
+++ src/cho/jaeseong/calendar/Calendar.java
@@ -1,10 +1,37 @@
 package cho.jaeseong.calendar;
 
 /*
- * 요구사항
-3단계와 같지만 년도와 월만 입력받는다.
-진짜 캘린더에서 나오는 달력과 똑같은 모양의 달력을 출력한다.
-추가적으로 입력받아야 하는 내용이 있는지 생각해 보자
+요구 사항
+간단한 콘솔 기반 사용자 UI를 만든다. (일정 검색용)
+오늘 날짜를 인식해서 해당 월의 달력을 출력한다.
+달력에서 일정이 있는 날 아래에는 . 을 찍어서 보여준다.
+일정을 등록, 검색, 변경이 가능하게 해 준다.
+오늘의 일정을 표시해 준다.
+
+---입력 및 출력 예시---
+
++----------------------+
+| 1. 일정 등록           
+| 2. 일정 검색           
+| 3. 달력 보기
+| h. 도움말 q. 종료
++----------------------+
+명령 (1, 2, 3, h, q)
+> 1
+[일정 등록] 날짜를 입력하세요.
+> 2016-06-05
+일정을 입력하세요.
+> 자바지기에게 밥 얻어먹기
+일정이 등록되었습니다.
+명령 (1, 2, 3, h, q)
+> 2
+[일정 검색] 날짜를 입력하세요.
+> 2016-06-05
+1개의 일정이 있습니다.
+1. 자바지기에게 밥 얻어먹기
+명령 (1, 2, 3, h, q)
+q
+Bye
  */
 
 public class Calendar {
@@ -49,12 +76,6 @@ public class Calendar {
 	private int getWeekDay(int year, int month, int day) {
 		int syear = 1970;
 		int smonth = 1;
-		// 9. 안쓴 상수or변수는 삭제한다.
-		//int sday = 1; 
-		
-		//5. 상수값으로 변경(final+대문자)
-		//int standardWeekday = 3;
-//		final int STANDARD_WEEKDAY= 3; // 12. 목요일을 4로 수정 
 		final int STANDARD_WEEKDAY= 4; 
 		int count = 0;
 
@@ -69,45 +90,13 @@ public class Calendar {
 
 		count += (day - 1);
 
-		// 2.
-		// System.out.println(count);
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
 		
-		//4.이제 지난일수count를 가지고, 기준일의 요일 기준으로, 무슨요일인지 판단해야한다.
-		//int weekday = count % 7; // 지난일0 -> 0, 1일지남 -> 1 ... ===> 지난일0일때 기준시작요일(standardWeekday)이 되도록해야한다.
-		// - 기준요일을 final상수+대문자로 변경하고 그 상수값을 가져온다.
-		//6. 달력출력할때처럼, 7개씩 돌아가면 %7로서 0~6주기를 구하되, 시작점이 오른쪽으로 땡겨간다면, [ 그만큼 먼저 더해놓고 ] % 7을 시행한다. 
-		int weekday = (count + STANDARD_WEEKDAY) % 7; // 지난일0 -> 0, 1일지남 -> 1 ... ===> 지난일0일때 기준시작요일(standardWeekday)이 되도록해야한다.
-		
-		//7.
-//		return count;
 		return weekday;
 	}
 
-	// simple test code here
 	public static void main(String[] args) {
-		Calendar cal = new Calendar();
-		// 1. getWeekDay라는 기준일로부터 지속일을 함수내부에서 찍는게 아니라 return해놓고, 메인에서 찍도록 수정하자. -> 2.
-		// 3. **Junit 테스트처럼 예상값을 미리 넣어놓고 비교하여 t/f를 반환하게 찍는다..**
-//		cal.getWeekDay(1970, 1, 1);
-//		System.out.println(cal.getWeekDay(1970, 1, 1) == 0);
-//		System.out.println(cal.getWeekDay(1970, 2, 1) == 31); // 2월까지의 차이는... 직전의1월이 전체지난 거다.. 윤년상관없이 31일..
-//		System.out.println(cal.getWeekDay(1970, 3, 1) == (31 + 28)); // 1970년은 윤년X -> 2월달은 28일
-//		/*
-//		 * true true true
-//		 */
-//		System.out.println(cal.getWeekDay(1971, 1, 2) == (365 + 1)); // 1971년은.. 직전인 1970년이 간 것 -> 1970년이 윤년이냐?
-		
-		//10. 이제 [예상되는] 지난일이 아닌 [요일]을 정답초롬 입력해서 t/f를 뽑아보자.
-		System.out.println(cal.getWeekDay(1970, 1, 1) == 0+4); // 당일안지났으면 최초의 목요일 0123 -> 01234
-//		System.out.println(cal.getWeekDay(1970, 2, 1) == 0);  // 다음달은 바로 안나온다.
-		System.out.println(cal.getWeekDay(1971, 1, 1) == 0+4+1); // but 1년지난 것은 365를 7로 나누면 나머지가 1이 되니, 7개씩 까지고 1일 지난 금요일이다. 
-		System.out.println(cal.getWeekDay(1972, 1, 1) == 0+4+2); // * 윤년이 아닌 이상 1년지나면 해마다+1일씩 요일이 나아간다*  (윤년은 +2씩 간다)
-		System.out.println(cal.getWeekDay(1973, 1, 1) == 0+4+3); // 1973->1972년을 낀 해 -> 윤년이라서 +2가 늘어났을 것이다.
-		System.out.println(cal.getWeekDay(1973, 1, 1) == 1); // 4+4->**8 -> 1로 답 수정 **  true
-		
-		// 11. -> 이제 Prompt에 가서 실행시키자!!(여기는 해당class 테스트용main)
-		// 12. weekday 잘못계산한것(Prompy에서는 0이 일요일 -> Calendar는 0이 월요일.. -> 목요일을 3에서 4로 수정해주자.)
-		
+//		Calendar cal = new Calendar();
 	}
 
 }
--- src/cho/jaeseong/calendar/Prompt.java
+++ src/cho/jaeseong/calendar/Prompt.java
@@ -3,6 +3,20 @@ package cho.jaeseong.calendar;
 import java.util.Scanner;
 
 public class Prompt {
+	
+	//1. UI부터 만들어주기 위해 메소들 정의함.
+	// - 메소드 같은 경우, main이라도,  객체.메서드()로 호출할 예정이므로 static필요없고 public으로 호출만 되게 한다.(private만 아니면 객체.메서드()호출될듯)
+	public void printMenu() {
+		// 2. 프롬프트에 찍힐 텍스트를 그대로 복사해온 뒤, 앞에 println 만 붙혀주면 그대로 출력됨.
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");           
+		System.out.println("| 2. 일정 검색");           
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+		//System.out.println("명령 (1, 2, 3, h, q)");
+	}
+	
 	/**
 	 * 
 	 * @param week 요일명
@@ -31,36 +45,120 @@ public class Prompt {
 	}
 
 	public void runPrompt() {
+		//3. 필드들 선언하기도 전에 먼저 ui부터 출력되도록하기 -> 메인 메소드에서, printMenu()인스턴스 메소드 호출..이.. 아닌.. 
+		// -> 첫 호출되는 p.runPrompt()[인스턴스메소드]안에서..[인스턴스메소드]printMenu()를 편하게 .. 바로 편하게.. 호출하게하네.. 나는 인스턴스를 의미하는 this붙혀주기 
+		this.printMenu();
+		
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
+		// 11. 달력input받는 용이라서 따로빼준 메소드 내부로 가지고 들어가기
+//		int month = 1;
+//		int year = 2000;
+		
+		while (true) {
+			//4. 이제 [무한루프속에서 q할대까지 반복되지만] 달력보기의 전에, 1글자를 입력을 먼저 받아야한다.
+			//명령 (1, 2, 3, h, q)
+			System.out.println("명령 (1, 2, 3, h, q)"); // 입력받는 곳에서는 멈춘다. 그전에 안내멘트가 나가야한다. 특히 무한반복에서  매번 나올 안내문
+			String cmd = scanner.next();
+			//5. 각 받은 1글자마다 분기처리를 해줘야하는데, | 1. 일정 등록 | 2. 일정 검색 | 3. 달력 보기 | h. 도움말 q. 종료
+			//**method를 미리 주석으로? 아니면 에러나더라도 가상으로 작성해주고 -> [ctrl+1]으로 create 해당메소드로 만들어두자.**
+			// - switch case에 문자열이 안되서 if로 한다고 함. / if문은 한줄이면 중괄호 없애고 1line으로 올려도됨. 
+			if (cmd.equals("1")) {
+				cmdRegister();
+			} else if (cmd.equals("2")) {
+				cmdSearch();
+			} else if (cmd.equals("3")) {
+				//17. 파라미터 추가되었으니 인자로 넣어준다.
+//				cmdCal();
+				cmdCal(scanner, cal);
+			} else if (cmd.equals("h")) {
+				this.printMenu(); // 도움말은 최초 메뉴 그대로 한번 더 출력하면 된다.
+			} else if (cmd.equals("q")) {
+				break;
+			}
+			
+			
+		} // 6. 무한루프를 도는것은 메뉴UI만 돌아야한다. 달력 출력부분은 이제 무한반복 밖으로 뺀다. while(true)를 닫아버린다!!!
+  		  // -> 달력 출력하는 부분은 따로 메소드로 뺄 준비를 하고, 맨 밑에  bye+scanner.close()부분을 while문 안에 넣어준다.	
+
+		//8. 무한루프를 끝나고 나올 메뉸
+		System.out.println("Bye~!");
+		scanner.close();
+	}
+		
+			
+			// 7. 달력 작성부분을 cmdCal()로 옮기자.
+//			System.out.println("연도를 입력하세요.(exit:-1)");
+//			System.out.print("YEAR> ");
+//			year = scanner.nextInt();
+//			if (year == -1) {
+//				break;
+//			}
+//			System.out.println("달을 입력하세요.");
+//			System.out.print("MONTH> ");
+//			month = scanner.nextInt();
+//
+//			if (month == -1) {
+//				break;
+//			}
+//			if (!(1 <= month && month <= 12)) {
+//				System.out.println("1~12사이의 값을 입력하세요.");
+//				continue;
+//			}
+//			cal.printCalendar(year, month);
+//		} // 앞에서 닫음. 여긴 삭제해야할 괄호
+		
+
+
+
+//	private void cmdCal() {
+		//9. 옮기고 나니, 객체 scanner를 뺏긴 상태라 필요함. -> 메소드로 들어온 상황이니까, 앞에 있떤 scanner객체 를 파라미터로 받아서 쓰자.
+//	private void cmdCal(Scanner scanner) {
+	//14. cal객체도 필요하다. 메소드내에서 객체 만들지말고 밖에서 받자. 
+	// - scanner는 매번 close도 해줘야하며.. 함수호출 == 해당 기능시마다 객체를 생성하면 안된다.
+	// -> 객체는 메소드 내에서 쓸일이 있으면, 미리 밖에서 선언된 것을 파라미터로 받아야한다. 
+	private void cmdCal(Scanner scanner, Calendar cal) {
+//		System.out.println("연도를 입력하세요.(exit:-1)");
+		System.out.println("연도를 입력하세요.");
+		System.out.print("YEAR> ");
+		//10. 밖에 있을 때, input으로 들어올 month, year를 받아줄 변수를 미리 선언해놨었다. -> 가지고 들어오자.
 		int month = 1;
 		int year = 2000;
-
-		while (true) {
-			System.out.println("연도를 입력하세요.(exit:-1)");
-			System.out.print("YEAR> ");
-			year = scanner.nextInt();
-			if (year == -1) {
-				break;
-			}
-			System.out.println("달을 입력하세요.");
-			System.out.print("MONTH> ");
-			month = scanner.nextInt();
-
-			if (month == -1) {
-				break;
-			}
-			if (!(1 <= month && month <= 12)) {
-				System.out.println("1~12사이의 값을 입력하세요.");
-				continue;
-			}
-			cal.printCalendar(year, month);
+		year = scanner.nextInt();
+		//12. 여기서 이제 무한반복break할 없으니까, year, month로 -1체크한 것 + 멘트 + break부분 제거하자.
+//		if (year == -1) {
+//			break;
+//		}
+		System.out.println("달을 입력하세요.");
+		System.out.print("MONTH> ");
+		month = scanner.nextInt();
+//
+//		if (month == -1) {
+//			break;
+//		}
+		if (!(1 <= month && month <= 12)) {
+			System.out.println("1~12사이의 값을 입력하세요.");
+			//15. 범위검사에 걸리면 [무한반복내부가 아니므로 break처럼 continue도 제거]해야한다.
+			// -> 범위이상하게 들어오면, ***[ 지금 현재 함수를 종료시켜 바깥 작업중이던 곳으로 가기 ]*** 위해 ***return;***만
+			//continue;
+			return; // 함수 종료시켜 stack비우고 원래 메인으로 가기~
+			
 		}
+		//13. cal객체가 필요다 이것도 메소드 내에서 객체를 만들지말고, 파라미터로 받자. 
+		cal.printCalendar(year, month);
+		
+		//16. 이제 밖에서도 파라미터들을 인자로 받도록 수정해주러 가자.
+	}
 
-		System.out.println("Bye~!");
+	private void cmdSearch() {
+		// TODO Auto-generated method stub
+		
+	}
 
-		scanner.close();
+	private void cmdRegister() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args) {
