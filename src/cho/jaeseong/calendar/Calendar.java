8b23b5609f4c1cda28b8a4b403374f51aa6c84f8
is2js <tingstyle1@gmail.com>
2021-11-17 12:41:25 +0900

    refactor: parseDay() and while문 속 cmd마다 if else -> switch문으로 변경해보기  - flag를 이용해서 2중 반복문 중 바깥while빠져나가기

--- src/cho/jaeseong/calendar/Calendar.java
+++ src/cho/jaeseong/calendar/Calendar.java
@@ -5,39 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-/*
-요구 사항
-간단한 콘솔 기반 사용자 UI를 만든다. (일정 검색용)
-오늘 날짜를 인식해서 해당 월의 달력을 출력한다.
-달력에서 일정이 있는 날 아래에는 . 을 찍어서 보여준다.
-일정을 등록, 검색, 변경이 가능하게 해 준다.
-오늘의 일정을 표시해 준다.
-
----입력 및 출력 예시---
-
-+----------------------+
-| 1. 일정 등록           
-| 2. 일정 검색           
-| 3. 달력 보기
-| h. 도움말 q. 종료
-+----------------------+
-명령 (1, 2, 3, h, q)
-> 1
-[일정 등록] 날짜를 입력하세요.
-> 2016-06-05
-일정을 입력하세요.
-> 자바지기에게 밥 얻어먹기
-일정이 등록되었습니다.
-명령 (1, 2, 3, h, q)
-> 2
-[일정 검색] 날짜를 입력하세요.
-> 2016-06-05
-1개의 일정이 있습니다.
-1. 자바지기에게 밥 얻어먹기
-명령 (1, 2, 3, h, q)
-q
-Bye
- */
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -57,8 +24,6 @@ public class Calendar {
 	 */
 	public void registerPlan(String strDate, String plan) throws ParseException {
 		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-		//System.out.println(date);
-		
 		this.planMap.put(date, plan);
 	}
 	
--- src/cho/jaeseong/calendar/Prompt.java
+++ src/cho/jaeseong/calendar/Prompt.java
@@ -13,6 +13,13 @@ public class Prompt {
 		System.out.println("| h. 도움말 q. 종료");
 		System.out.println("+----------------------+");
 	}
+	
+
+	/*
+	 * 1) switch case - String
+	 * 2) Plan class: 전 단계에서 구현한 내용을 클래스를 사용해서 다시 구현한다.
+	 *    PlanItem 클래스를 만든다. -> refactor다.
+	 */
 
 	/**
 	 * 
@@ -20,25 +27,46 @@ public class Prompt {
 	 * @return 0 ~ 6 (0 = Sunday, 6= Saturday)
 	 */
 	public int parseDay(String week) {
-		if (week.equals("SU")) {
+		// 1. if else if -> switch case 문 변경해보기
+		// java 1.7(JDK 7이상?)부터는 문자열을 변수로 받을 수 있다고 한다.
+		// 각 경우마다 break 대신 return을 써주면 함수 탈출이므로 break 대신 return을 switch case문에 쓴다.
+		switch (week) {
+		case "SU":
+			return 0;
+		case "MO":
+			return 1;
+		case "TU":
+			return 2;
+		case "WE":
+			return 3;
+		case "TH":
+			return 4;
+		case "FR":
+			return 5;
+		case "SA":
+			return 6;
+		default:
 			return 0;
 		}
-		if (week.equals("MO")) {
-			return 1;
-		}
-		if (week.equals("TU")) {
-			return 2;
-		}
-		if (week.equals("WE")) {
-			return 3;
-		}
-		if (week.equals("TH")) {
-			return 4;
-		}
-		if (week.equals("FR")) {
-			return 5;
-		}
-		return 6;
+//		if (week.equals("SU")) {
+//			return 0;
+//		}
+//		if (week.equals("MO")) {
+//			return 1;
+//		}
+//		if (week.equals("TU")) {
+//			return 2;
+//		}
+//		if (week.equals("WE")) {
+//			return 3;
+//		}
+//		if (week.equals("TH")) {
+//			return 4;
+//		}
+//		if (week.equals("FR")) {
+//			return 5;
+//		}
+//		return 6;
 	}
 
 	public void runPrompt() throws ParseException {
@@ -47,32 +75,62 @@ public class Prompt {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		while (true) {
+		// 5. 2중 반복(여기선 반복-switchcase)를 빠져나오기 위해서는 
+		//-> 1) 백기선 스터디 outer에 이름적어줘서 그걸로빠져나가기??
+		//-->   outer: while (true) {    --->   case "q": break outer;  ---> 됨.
+		//-> 2) 고전방식: boolean isLoop = true의 flag변수 생성후, while (flag) -> 해당경우에서 flag = false로 업데이트 -> 담 루프 시작전 종료
+		//->    cf) 해당레벨까지 BFS:  count = 0  -> while not count -> 해당레벨중간에 count +=1로 업데이트 ->  담 루프 시작전 종료
+		//-->  만약, whiel (flag)가 불가능한 for문이라면? 처음시작부터  for  if not flag==true:break 달려있을 듯?
+//		outer: while (true) {
+//		while (true) {
+		//6. 5-2의 플래그변수로 이중 반복문 빠져나오기
+		boolean isLoop = true;
+//		while (true) {
+		while (isLoop) {
 			System.out.println("명령 (1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			if (cmd.equals("1")) {
-				// 1. 일정등록 로직으로 넘어갈 때, 역시나 ***[[ Prompt시작시 만드는]]]*** scanner객체 재활용 +
-				// cal객체(cal.일정등록/검색 in hashmap) 등 객체들이 필요할 것이다.
-				// cf) 객체는 메소드로 넘어가기 전에 미리 생성 -> 인자로 갖다써서 재활용
-				// cmdRegister();
-				cmdRegister(scanner, cal); // -> 2. F3눌러서 파라미터 추가해주기
-			} else if (cmd.equals("2")) {
-				// 5. 이제 일정검색 연걸해주기 -> 마찬가지로 입력을받고 (Scanner객체 사용) 캘린더 기능(탐색기능 Calendar객체)를 이용할
-				// 예정이니 인자-> 파라미터로 넣어준다.
-				// cmdSearch();
+			//2 . 1,2,3 만 보면 Integer.parseInt써야할 것 같지만, h, q가 섞여있는 경우 문자열로 받아 처리해야함.
+			switch (cmd) {
+			case "1":
+				cmdRegister(scanner, cal);
+				//3. if - else if와 다르게, 서로 배반의 상황임을 [case마다 배반]처리하기 위해서는 [추가] break처리가 필요하다.
+				// -> while문 처럼, swtich도 break로 빠져나간다!! 
+				break;
+			case "2":
 				cmdSearch(scanner, cal);
-			} else if (cmd.equals("3")) {
+				break;
+			case "3":
 				cmdCal(scanner, cal);
-			} else if (cmd.equals("h")) {
+				break;
+			case "h":
 				this.printMenu();
-			} else if (cmd.equals("q")) {
+				break;
+			case "q":
+				// 4. 여기서는 swtich case뿐만 아니라 whlie문도 빠져나가야하는데, break 1개로는.. switch case밖에 못빠져나간다.
+				//break outer;
+				// 6-2. isLoop의 flag만 false로 업데이트해주면, while (flag) 로인해 바깥 반복문이 다음루프 떄멈춘다.
+				isLoop = false; // 지금 당장이 아니라, 다음루프 검사 때 멈춤!
 				break;
 			}
 		}
-
-		System.out.println("Bye~!");
+		System.out.println("Bye~!"); 
 		scanner.close();
 	}
+
+		
+//			if (cmd.equals("1")) {
+//				cmdRegister(scanner, cal); 
+//			} else if (cmd.equals("2")) {
+//				cmdSearch(scanner, cal);
+//			} else if (cmd.equals("3")) {
+//				cmdCal(scanner, cal);
+//			} else if (cmd.equals("h")) {
+//				this.printMenu();
+//			} else if (cmd.equals("q")) {
+//				break;
+//			}
+//		}
+
 
 	private void cmdCal(Scanner scanner, Calendar cal) {
 		System.out.println("연도를 입력하세요.");
@@ -91,54 +149,32 @@ public class Prompt {
 
 	}
 
-	// 6. 필요한 객체를 받는 파라미터를 추가해주고 함수를 구현부를 작성해준다.
 	private void cmdSearch(Scanner scanner, Calendar cal) {
 		System.out.println("[일정 검색]");
 		System.out.println("날짜를 입력해 주세요(yyyy-MM-dd).");
 		String date = scanner.next();
-		// 7. 이번에는 trycatch로 해주자.
-		// String plan;
-		// 8. 에러가 날 경우, plan이 초기화가 안된 상태로 println(plan)된다고 에러뜬다. -> 빈 문자열로 초기화해놓자.
 		String plan = "";
 		try {
 			plan = cal.searchPlan(date);
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			// 9. 에러는 syserr로 찍자
 			System.err.println("일정 검색 중 오류가 발생했습니다.");
 		}
 		System.out.println(plan);
 
 	}
 
-	// 2. Prompt에서 cal.일정등록/검색 메소드을 호출하도록 연결해주는 메소드를 정의해주자.
-//	private void cmdRegister() {
 	private void cmdRegister(Scanner scanner, Calendar cal) throws ParseException {
-		// 3. 여기서는 일단 다시 안내문과 함계 등록할 일정날짜(strDate)를 입력을 받아야한다.
-		System.out.println("[새 일정 등록] ");// 한줄로 표시..?
+		System.out.println("[새 일정 등록] ");
 		System.out.println("날짜를 입력해 주세요(yyyy-MM-dd).");
 		String date = scanner.next();
 		System.out.println("일정을 입력해주세요");
-//		//10. 테스트과정에서 nextLine();으로 긴 문장을 받을 수 없다고 한다. -> 한단어씩 word로 받고 + " " 공백 넣어주고 + ;세미콜론들어오면 break하는 무한반복으로 단어받기
-//		//String text = scanner.nextLine();
-//		String text = "";
-//		while (true) {
-//			String word = scanner.next();
-//			text += word + " ";
-//			if (word.endsWith(";")) {
-//				break;
-//			}
-//		}
-		//11. 최종수정본
 		String text = "";
-		scanner.nextLine(); // 입력받은 1줄을 날림 -> 이유는 모름
+		scanner.nextLine(); 
 		text = scanner.nextLine();
 		
-		
 
-		// 4. strDate와 plan 문자열을 받았으면 cal.일정등록 기능을 이용한다.
-		cal.registerPlan(date, text); // throws까지 처리해줘야함. -> 연쇄적으로.. 담부턴 try catch로!@!
+		cal.registerPlan(date, text); 
 
 	}
 
